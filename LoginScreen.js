40cd43fa667b8ee62b324bff46fa5cb2480a73b6
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-17 17:18:59 -0300

    Acréscimo de @hookform/resolvers

--- LoginScreen.js
+++ LoginScreen.js
@@ -3,6 +3,7 @@ import { Alert, View, TextInput, Text, Button } from 'react-native';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import styles from './LoginScreen.style';
+import { yupResolver } from "@hookform/resolvers/yup";
 
 const fieldValidationSchema = yup.object().shape({
 	email: yup
@@ -16,7 +17,8 @@ const fieldValidationSchema = yup.object().shape({
 })
 
 const LoginScreen = () => {
-	const { register, setValue, handleSubmit, errors } = useForm ({validationSchema: fieldValidationSchema});
+	const { register, setValue, handleSubmit, formState: { errors } } = useForm ({ resolver: yupResolver(fieldValidationSchema) });
+	//const { register, setValue, handleSubmit, errors } = useForm ({ validationSchema: fieldValidationSchema });
     //const onSubmit = (data) => Alert.alert(data.email, data.password);
     const onSubmit = (data) => console.log(data.email, data.password);
 
@@ -28,23 +30,24 @@ const LoginScreen = () => {
     return (
 		<View style={styles.mainContainer}>
 			<TextField
-			label={'Email'}
-            error={errors?.email}
-			placeholder={'Digite seu email'}
-            onChangeText={text => setValue("email", text)}
+				label={'Email'}
+            	error={errors?.email}
+				placeholder={'Digite seu email'}
+            	onChangeText={text => setValue("email", text)}
 			/>
 			<TextField
-			label={'Senha'}
-            error={errors?.password}
-			placeholder={'Digite sua senha'}
-            onChangeText={text=> setValue("password", text)}
+				label={'Senha'}
+            	error={errors?.password}
+				placeholder={'Digite sua senha'}
+            	onChangeText={text=> setValue("password", text)}
 			/>
+
 			<Button title='Continuar' onPress={handleSubmit(onSubmit)} />
         </View>
 	)
 }
 
-const TextField= ({ error, label, ...inputProps}) => (
+const TextField= ({ error, label, ...inputProps }) => (
 	<View style={styles.container}>
 	    <Text style={styles.label}>{label}</Text>
 	    <TextInput
